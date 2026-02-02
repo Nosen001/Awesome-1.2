278ae6df57c1e1b4e57ff5d4f1effc53a3993245
Senpai  Nosen <nosen.inc@outlook.com>
2026-02-02 20:37:22 +0100

    Initial Commit

---
+++ Awesome 1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Awesome 1.2
+//
+//  Created by secjedi on 02.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Awesome 1.2UITests/Awesome_1_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Awesome_1_2UITestsLaunchTests.swift
+//  Awesome 1.2UITests
+//
+//  Created by secjedi on 02.02.26.
+//
+
+import XCTest
+
+final class Awesome_1_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
